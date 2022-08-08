4d11434c862ec478ee306e7e94f76414b6a892cc
tianhaozhang <zhangtianhao@tezign.com>
2022-08-08 17:13:03 +0800

    fix codes

--- src/utils/utlis.py
+++ src/utils/utlis.py
@@ -3,20 +3,20 @@ import os
 
 def GetParentPath(strPath):
     if not strPath:
-        return None;
+        return None
 
-    lsPath = os.path.split(strPath);
+    lsPath = os.path.split(strPath)
     if lsPath[1]:
-        return lsPath[0];
+        return lsPath[0]
 
-    lsPath = os.path.split(lsPath[0]);
-    return lsPath[0];
+    lsPath = os.path.split(lsPath[0])
+    return lsPath[0]
 
 def modify_file(file,rstr):
     try:
         index_file = open(file,'w')
         index_file.write(str(rstr))
         index_file.close()
-    except Exception,e:
-        print e
+    except Exception as e:
+        print(e)
 
